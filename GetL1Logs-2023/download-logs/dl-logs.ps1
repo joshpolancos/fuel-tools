bab622ae1f3b3c46c91dbeb7772fbf97e91f7ba1
joshpolancos <55184826+joshpolancos@users.noreply.github.com>
2023-04-13 20:52:40 +0800

    Delete GetL1Logs-2023 directory

--- GetL1Logs-2023/download-logs/dl-logs.ps1
+++
@@ -1,9 +0,0 @@
-﻿param($mname)
-$dlStart = "Downloading logs from $mname"
-$dlFinish = "Download complete - $mname"
-
-$host.ui.RawUI.WindowTitle = $dlStart
-copy "\\$mname\c$\temp\jcp-getl1logs\$mname-logs.zip" .\download-logs -Force
-dir ".\download-logs\$mname-logs.zip"
-$host.ui.RawUI.WindowTitle = $dlFinish
-pause
